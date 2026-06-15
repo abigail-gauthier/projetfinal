93b5ac903ece9e600ebb3cf9a8b2068271bf4af7
Abigail gauthier <abigail.r.gauthier@gmail.com>
2026-06-15 17:08:16 -0400

    Version 1 restored

--- index-2.js
+++ index-2.js
@@ -1,4 +1,1 @@
-console.log("This is the change here!!!!!!!!!!!!!!!!!!!!!!");
-console.log("version 2");
-console.log("version 3");
-console.log("reset is here");
+console.log("This is the change here");
--- index.js
+++ index.js
@@ -1,5 +1,3 @@
 console.log("Hello World");
 console.log("this works!");
 console.log("version 2");
-console.log("version 3");
-console.log("reset is here");
