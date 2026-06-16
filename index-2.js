20f495332cb6b56cab775685079a56d03ed8599b
Abigail gauthier <abigail.r.gauthier@gmail.com>
2026-06-16 07:44:19 -0400

    This is the new local repo version

--- index-2.js
+++ index-2.js
@@ -3,3 +3,5 @@ console.log("version 2");
 console.log("version 3");
 console.log("reset is here");
 console.log ("This is version 12");
+
+console.log("This message is from the new local repository");
