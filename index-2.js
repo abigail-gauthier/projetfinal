5c3d8d20b0b06fdcbadaee4dac57189e78cb38e1
Abigail gauthier <abigail.r.gauthier@gmail.com>
2026-06-16 06:15:15 -0400

    version 12

--- index-2.js
+++ index-2.js
@@ -2,3 +2,4 @@ console.log("This is the change here!!!!!!!!!!!!!!!!!!!!!!");
 console.log("version 2");
 console.log("version 3");
 console.log("reset is here");
+console.log ("This is version 12");
--- index.js
+++ index.js
@@ -3,3 +3,4 @@ console.log("this works!");
 console.log("version 2");
 console.log("version 3");
 console.log("reset is here");
+console.log ("This is version 12");
